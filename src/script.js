b1272f04aaa249f661e2ab316da1df446d038374
aurelien.hmdy <aurelien@hemidy.fr>
2022-06-10 08:36:08 +0200

    :construction: Added arc stroked particles

--- src/script.js
+++ src/script.js
@@ -1,12 +1,12 @@
-import "./style.css";
-import gui from "lil-gui";
-import gsap from "gsap";
+import './style.css';
+import gui from 'lil-gui';
+import gsap from 'gsap';
 
-const canvas = document.querySelector(".canvas");
+const canvas = document.querySelector('.canvas');
 
 const GUI = new gui();
 
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext('2d');
 
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -30,48 +30,48 @@ const parameters = {
 
 let time = new Date();
 
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 });
 
-window.addEventListener("mousemove", (event) => {
+window.addEventListener('mousemove', (event) => {
   mouse.x = event.clientX - canvas.offsetLeft;
   mouse.y = event.clientY - canvas.offsetTop;
   // mouse.x = window.pageXOffset + event.clientX - canvas.offsetLeft;
   // mouse.y = window.pageYOffset + event.clientY - canvas.offsetTop;
 });
 
-window.addEventListener("click", () => {
-  console.log("blbla");
+window.addEventListener('click', () => {
+  console.log('blbla');
   parameters.centerCircle = !parameters.centerCircle;
   if (parameters.centerCircle) {
-    gsap.to(parameters, { lerp: 1, duration: 1, ease: "pow2.out" });
+    gsap.to(parameters, { lerp: 1, duration: 1, ease: 'pow2.out' });
   } else {
-    gsap.to(parameters, { lerp: 0, duration: 1, ease: "pow2.out" });
+    gsap.to(parameters, { lerp: 0, duration: 1, ease: 'pow2.out' });
   }
 });
 
-ctx.fillStyle = "rgb(250, 250, 0)";
-ctx.strokeStyle = "rgb(250, 102, 40)";
+ctx.fillStyle = 'rgb(250, 250, 0)';
+ctx.strokeStyle = 'rgb(250, 102, 40)';
 
 // Add points on circle
 const pointsNumber = 20;
 let radius = 300;
 let rotate = 0;
 
-GUI.add(parameters, "radius").min(0).max(600).step(1);
-GUI.add(parameters, "amplitude").min(0).max(2).step(0.01);
-GUI.add(parameters, "lerp").min(0).max(1).step(0.01);
-GUI.add(parameters, "centerCircle").onChange((value) => {
+GUI.add(parameters, 'radius').min(0).max(600).step(1);
+GUI.add(parameters, 'amplitude').min(0).max(2).step(0.01);
+GUI.add(parameters, 'lerp').min(0).max(1).step(0.01);
+GUI.add(parameters, 'centerCircle').onChange((value) => {
   if (value) {
-    gsap.to(parameters, { lerp: 1, duration: 1, ease: "pow2.out" });
+    gsap.to(parameters, { lerp: 1, duration: 1, ease: 'pow2.out' });
   } else {
-    gsap.to(parameters, { lerp: 0, duration: 1, ease: "pow2.out" });
+    gsap.to(parameters, { lerp: 0, duration: 1, ease: 'pow2.out' });
   }
 });
-GUI.add(parameters, "rotation");
-GUI.add(parameters, "rotate")
+GUI.add(parameters, 'rotation');
+GUI.add(parameters, 'rotate')
   .min(0)
   .max(Math.PI * 2)
   .step(0.01);
@@ -83,15 +83,14 @@ const center = {
 const drawCircle = (radius, rotation) => {
   for (let i = 0; i < pointsNumber; i++) {
     const position = {
-      x:
-        radius * Math.cos(((Math.PI * 2) / pointsNumber) * i + rotation) +
-        center.x,
-      y:
-        radius * Math.sin(((Math.PI * 2) / pointsNumber) * i + rotation) +
-        center.y,
+      x: radius * Math.cos(((Math.PI * 2) / pointsNumber) * i + rotation) + center.x,
+      y: radius * Math.sin(((Math.PI * 2) / pointsNumber) * i + rotation) + center.y,
     };
-    ctx.fillStyle = "rgb(250, 250, 0)";
-    ctx.fillRect(position.x, position.y, 10, 10);
+    ctx.strokeStyle = 'rgb(250, 250, 0)';
+    // ctx.fillRect(position.x, position.y, 10, 10);
+    ctx.beginPath();
+    ctx.arc(position.x, position.y, 5, 0, Math.PI * 2);
+    ctx.stroke();
     // ctx.arc(position.x, position.y, 10, 0, Math.PI * 2, false);
   }
 };
@@ -119,7 +118,7 @@ const frequency = 0.001;
 
 const tick = () => {
   // ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-  ctx.fillStyle = "rgba(0, 0, 0, 0.6";
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.6';
   ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
 
   let elapsedTime = new Date() - time;
